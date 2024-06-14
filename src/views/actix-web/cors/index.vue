3b12e655b3711d64382cf8ad5f42bde4a7aa6b73
yaoxingpu <yaoxpu@163.com>
2024-06-14 09:21:48 +0800

    update: 修改env配置

---
+++ src/views/actix-web/cors/index.vue
@@ -0,0 +1,12 @@
+<!--
+ * @Author: yaoxingpu yaoxpu@163.com
+ * @Date: 2024-06-03 15:40:26
+ * @LastEditors: yaoxingpu yaoxpu@163.com
+ * @LastEditTime: 2024-06-03 15:42:37
+ * @FilePath: /vue-naive-admin/src/views/actix-web/cors/index.vue
+ * @Description:
+ *
+-->
+<template>
+  <div>你好</div>
+</template>
